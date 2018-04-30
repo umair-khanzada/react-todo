43995724baeaf4afc7f912cf86f759087d0d732f
umair ahmed <umairkz52@gmail.com>
2018-04-30 19:29:58 +0500

    added todo heading.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = [
+      {id: 1, text: 'Do something.', completed: false},
+      {id: 2, text: 'Build todo app.', completed: false}
+    ];
+  }
+
   render() {
     return (
-      <div className="">
+      <div className="container">
+        <h2>Todo List</h2>
       </div>
     );
   }
